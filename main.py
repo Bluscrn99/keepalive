d500175488b25dcfa7ce0f417eeccac633c4015f
Bluscrn <114532380+Bluscrn99@users.noreply.github.com>
2025-09-20 21:46:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,29 @@ import os
 
 URL_TO_PING = os.environ.get('pingurl')
 
-while True:
-    try:
-        requests.get(URL_TO_PING, timeout=5)
-        print("Ping!")
-    except Exception as e:
-        print("Bonk. ", e)
-    time.sleep(60)  # ping every minute
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Dummy web server"
+
+@app.route("/ping")
+def ping():
+    print("Pong!")
+    return "Pong!", 200
+
+def ping():
+    while True:
+        try:
+            requests.get(URL_TO_PING, timeout=5)
+            print("Ping!")
+        except Exception as e:
+            print("Bonk. ", e)
+        time.sleep(60)  # ping every minute
+# run em
+if __name__ == "__main__":
+    threading.Thread(target=ping, daemon=True).start()
+    # Run Flask web server on the port Render expects
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
